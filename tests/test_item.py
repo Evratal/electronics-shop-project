b459e7117fe9a5a1e46e35e2f850aaf7a32d80f9
Evgeniy Ratuev <evratal@yandex.ru>
2023-09-16 22:46:37 +0400

    Update class Item: init,calculate_total_price and test her

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,8 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+from src.item import Item
+
+nout = Item("Ноутбуки", 20000, 100)
+
+
+def test_calculate_total_price():
+    assert nout.calculate_total_price() == nout.price * nout.quantity
